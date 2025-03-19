7d9f9841b8456f0266ccbd9b70dbe5f7dd6013fc
John-Henry Lim <42513874+Interpause@users.noreply.github.com>
2025-03-19 16:14:44 +0800

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# DAI-PDS Project
+
+Folder structure:
+
+- `dai-pds/`: Contains arduino code.
+- `firebase/`: Contains jupyter notebook to sign a JWT for the arduino to use for realtime database access.
---
+++ dai-pds/dai-pds-secret.template.h
@@ -0,0 +1,9 @@
+#define WIFI_SSID ""
+#define WIFI_PASSWORD ""
+
+#define DATABASE_URL "https://dai-pds-cb4fb-default-rtdb.asia-southeast1.firebasedatabase.app"
+// See: https://github.com/mobizt/FirebaseClient?tab=readme-ov-file#types-of-service-accounts (its hidden in a dropdown)
+// From dai-pds-cb4fb-firebase-adminsdk-fbsvc-3f195b93cd.json:
+#define FIREBASE_PROJECT_ID ""         // Taken from "project_id" key in JSON file.
+#define FIREBASE_CLIENT_EMAIL ""       // Taken from "client_email" key in JSON file.
+const char PRIVATE_KEY[] PROGMEM = ""; // Taken from "private_key" key in JSON file.
